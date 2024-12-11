5b4da5ba90df2bdb014c66ff3769e50732c8af6f
andre123354cs <edwar.vaco@gmail.com>
2024-12-11 09:44:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# URL del archivo CSV en GitHub
+url = "https://raw.githubusercontent.com/tu_usuario/tu_repositorio/rama/tu_archivo.csv"
+
+# Cargar el archivo CSV
+df = pd.read_csv(url)
+
+# Mostrar el contenido del archivo CSV
+st.write(df)
