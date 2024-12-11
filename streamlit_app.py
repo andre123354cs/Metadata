f11d1a006443714fe0a05e5d6e35625c74371f8b
andre123354cs <edwar.vaco@gmail.com>
2024-12-11 11:21:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,11 @@ import pandas as pd
 gsheetid='1YIvDyrXcDBuz8-NldwlA8bH7lsi6jf3-'
 sheetod='805003045'
 url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
+Comfama= pd.read_csv(url)
+st.dataframe(Comfama, use_container_width=True)
 
-dfDatos= pd.read_csv(url)
-
-st.dataframe(dfDatos, use_container_width=True)
+gsheetid='1YIvDyrXcDBuz8-NldwlA8bH7lsi6jf3-'
+sheetod='805003045'
+url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
+LD= pd.read_csv(url)
+st.dataframe(LD, use_container_width=True)
