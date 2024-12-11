aeda2803ef3744d92118a1e04c0514928fbea393
andre123354cs <edwar.vaco@gmail.com>
2024-12-11 11:18:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-gsheetid='1YIvDyrXcDBuz8-NldwlA8bH7lsi6jf3-'
+gsheetid='1YIvDyrXcDBuz8-NldwlA8bH7lsi6jf3'
 sheetod='805003045'
 url = f'https://docs.google.com/spreadsheets/d/{gsheetid}/export?format=csv&gid={sheetod}&format'
 
