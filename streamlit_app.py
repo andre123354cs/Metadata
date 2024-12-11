9f363a891fc4d91bf0cd4c66992a3ab0622b466a
andre123354cs <edwar.vaco@gmail.com>
2024-12-11 09:45:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # URL del archivo CSV en GitHub
-url = "https://raw.githubusercontent.com/tu_usuario/tu_repositorio/rama/tu_archivo.csv"
+url = "https://yesbpo201-my.sharepoint.com/:x:/g/personal/bi_yesbpo201_onmicrosoft_com/ESYDoH4rPDJLtaICGzphM98BXa_lqjTBjMUxSixhmbp9Cg?e=Sz5INZ"
 
 # Cargar el archivo CSV
 df = pd.read_csv(url)
