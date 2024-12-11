877c2d44edbcb671bbeda96ddefb3165ede7684b
andre123354cs <edwar.vaco@gmail.com>
2024-12-11 11:51:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,12 +83,8 @@ url_carteras = {
     #"Dolce": r"C:\\Users\\felip\\OneDrive\\Documentos\\Matris\\Dolce_Actualizacion.csv",
 }
 def cargar_parquet(url):
-        return pd.read_excel(url)
+    return pd.read_excel(url)
 
-  def cargar_parquet(ruta):
-      return pd.read_excel(ruta)
-
- 
       cartera_seleccionada = st.selectbox("Selecciona Alguna Cartera para descargar actualizacion: ", list(Fechas_Creacion.keys()))
       
       if cartera_seleccionada:
